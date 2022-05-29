fdf2c880ff2881eb48a9eecc5b7c66bba173d2bb
Ilya Poklitar <vaikus.gm@gmail.com>
2022-05-29 18:59:16 +1100

    edit test.js

--- test.js
+++ test.js
@@ -24,3 +24,48 @@ console.log([] + 1 + 2);
 console.log(!!(1 && 2));
 
 console.log('Ёжик' > 'яблоко');
+
+function findMaxNumber(num1, num2, num3, num4) {
+    console.log(num4);
+    if (isNaN(num1) || isNaN(num2) || isNaN(num3) || isNaN(num4)) {
+        return 0;
+    }
+    return Math.max(...[num1, num2, num3, num4]);
+}
+
+console.log(findMaxNumber(1,5,3));
+
+
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    
+    showAgeAndLangs: function(personalPlanPeter) {
+        const {languages} = personalPlanPeter.skills;
+        return `Мне ${personalPlanPeter.age} и я владею языками: ${languages.join(' ')}`;
+    }
+};
+
+function showExperience(personalPlanPeter) {
+    const {exp} = personalPlanPeter.skills;
+    return exp;
+}
+
+function showProgrammingLangs(personalPlanPeter) {
+    const {programmingLangs} = personalPlanPeter.skills;
+    let str = '';
+    
+    for (let key in programmingLangs) {
+        str = str + `Язык ${key} изучен на ${programmingLangs[key]}\n`
+    }
+    
+    return str;
+}
